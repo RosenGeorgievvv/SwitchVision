84076de28cc03b149dff265887766985c33c2bd6
Rosen Georgiev <rosengeorgiew1@gmail.com>
2023-05-15 12:24:25 +0300

    Update App.js
    This is a simple project, where you can switch between dark and light mode. It contains a Sign in form, but the main concept is using React Context for state management.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import Form from './components/Form';
 import { createContext, useState } from 'react';
+import ReactSwitch from 'react-switch';
 
 export const ThemeContext = createContext(null)
 
@@ -15,6 +16,10 @@ function App() {
     <ThemeContext.Provider value={{ theme, toggleTheme }}>
       <div className="App" id={theme}>
         <Form />
+        <div className='switch'>
+          <label>{theme === 'light' ? 'Light Mode' : 'Dark Mode'}</label>
+        <ReactSwitch onChange={toggleTheme} checked={theme === 'dark'}/>
+        </div>
       </div>
     </ThemeContext.Provider>
 
