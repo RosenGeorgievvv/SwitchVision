0ad3867b8b1be76ecb3ae97155c21963e0d7ca70
Rosen Georgiev <rosengeorgiew1@gmail.com>
2023-05-15 11:22:52 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import './App.css';
 import Form from './components/Form';
+import { createContext, useState } from 'react';
+
+export const ThemeContext = createContext(null)
 
 function App() {
+  const [theme, setTheme] = useState('light');
+  const toggleTheme = () => {
+
+    setTheme((curr) =>(curr === 'light' ? "dark" : "light"))
+  }
+
   return (
-    <div className="App" id='light'>
-      <Form />
-    </div>
+    <ThemeContext.Provider value={{ theme, toggleTheme }}>
+      <div className="App" id='light'>
+        <Form />
+      </div>
+    </ThemeContext.Provider>
+
   );
 }
 
