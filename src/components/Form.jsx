b9fa717ff464e1bd66e6daf028188792bc5d7e10
Rosen Georgiev <rosengeorgiew1@gmail.com>
2023-05-15 11:04:16 +0300

    structuring the project

---
+++ src/components/Form.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import '../styles/Form.css';
+
+function Form() {
+  return (
+    <div className='main'>
+        <p className='sign'>
+            Sign in
+        </p>
+        <form className='form'>
+            <input className='username' type='text' placeholder='Username' />
+            <input className='password' type='password' placeholder='Password' />
+            <a className='submit'>Sign in</a>
+            <p className='forgot'>
+                <a href='#'>Forgot Password?</a>
+            </p>
+        </form>
+    </div>
+  )
+}
+
+export default Form;
